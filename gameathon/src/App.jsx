48e83b714190e371dbf34d710678a73dfe13623e
Anuraag-Bagepalli <anuraag.rb@gmail.com>
2026-08-16 22:57:35 +0530

    fix(vercel): correct case-sensitive imports for Linux build environment

--- gameathon/src/App.jsx
+++ gameathon/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar.jsx';
-import Home from './pages/home';
-import Registration from './pages/registration';
-import Gallery from './pages/gallery';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Registration from './pages/Registration';
+import Gallery from './pages/Gallery';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import './App.css';
